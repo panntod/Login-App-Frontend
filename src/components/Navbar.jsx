d4b762ddcdecae533c60611da48aaa5b02ff595d
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-09 04:42:20 +0700

    [core] use selector, added hamburger menu, disable eslint

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import logo from "../logo.png";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { LogOut, reset } from "../features/authSlice";
 
 const Navbar = () => {
+  const { user } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const [isBurgerActive, setBurgerActive] = useState(false);
+
+  const toggleBurger = () => {
+    setBurgerActive(!isBurgerActive);
+  };
 
   const logout = () => {
     dispatch(LogOut());
@@ -22,17 +29,19 @@ const Navbar = () => {
         aria-label="main navigation"
       >
         <div className="navbar-brand">
-          <NavLink to="/dashboard" className="navbar-item ml-5">
-            <img src={logo} width="30" height="30" alt="logo" />
+          <NavLink to="/dashboard" className="navbar-item">
+            <img src={logo} width="30" height="30" className="ml-3" alt="logo" />
+            {user && <h2 className="title is-4 ml-3">{user.name}</h2>}
           </NavLink>
 
+           {/* eslint-disable-next-line */}
           <a
-            href="!#"
             role="button"
-            className="navbar-burger burger"
+            className={`navbar-burger burger${isBurgerActive ? " is-active" : ""}`}
             aria-label="menu"
-            aria-expanded="false"
+            aria-expanded={isBurgerActive}
             data-target="navbarBasicExample"
+            onClick={toggleBurger}
           >
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
@@ -40,7 +49,10 @@ const Navbar = () => {
           </a>
         </div>
 
-        <div id="navbarBasicExample" className="navbar-menu">
+        <div
+          id="navbarBasicExample"
+          className={`navbar-menu${isBurgerActive ? " is-active" : ""}`}
+        >
           <div className="navbar-end">
             <div className="navbar-item">
               <div className="buttons">
