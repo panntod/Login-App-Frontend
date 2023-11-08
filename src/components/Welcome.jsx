4f98058e431ca37b1005188477a19fb2b79a28b6
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-09 04:44:19 +0700

    [style] restyle dashboard

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,12 +3,20 @@ import { useSelector } from "react-redux";
 
 const Welcome = () => {
   const { user } = useSelector((state) => state.auth);
+
   return (
-    <div>
-      <h1 className="title">Dashboard</h1>
-      <h2 className="subtitle">
-        Welcome Back <strong>{user && user.name}</strong>
-      </h2>
+    <div className="section">
+      <div className="container">
+        <h1 className="title is-2">Dashboard</h1>
+        {user && (
+          <div className="box">
+            <h2 className="subtitle">
+              Welcome Back{" "}
+              <strong className="has-text-primary">{user.name}</strong>
+            </h2>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
